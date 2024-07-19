51320064ca9db71448daaf4290a03e86333f722d
rashmithachamikara <rashmitha.chamickara@gmail.com>
2024-07-19 23:31:24 +0530

    Added Propotional control to distance moving. Made compensation system for turn gaps

---
+++ Main Code/marquer13_L298N16_EncoderServoCommandlistsDistancePid/marquer13_L298N16_EncoderServoCommandlistsDistancePid.ino
@@ -0,0 +1,474 @@
+/*
+
+================= Marquer Main Code =========================
+
+Originally copied from: Components/L298N/"L298NserialSpeedAdjust16_MarkurWeb_Gyro_PID_Turn_Overshootv2"
+
+L298N Motor Control
+Display encoder feedback input
+PID for moving straight
+PID for turning
+Truning continously till turned 
+Tuned for turning
+Recover from stalling
+Add turn speed value as a global variable
+Changed PID system to run in realtime with 50ms dt clock
+Count Distance from wheel encoders
+Introuced a 50ms delay in main loop to avoid crashes and wifi usage induced power failures
+Send data over wifi in a 500ms delta clock interval
+Made the robot move a specified distance then stop
+Created a context system
+
+Todo
+- Calibrate distance moving system. Probably add proportional control
+
+===============================================================
+
+*/
+
+//Libraries
+#include "marquerWeb.h"
+#include "mpu9250.h" //By brain taylor 
+#include <ESP32Servo.h>
+#include "Ticker.h"
+
+//Pins
+#define IN1 25
+#define IN2 26
+#define IN3 27
+#define IN4 14
+#define ENA 12 //Right Motor Speed
+#define ENB 13 //Left Motor Speed
+
+//Alternative pin configuration for motors
+// #define ENA 25 //Right Motor Speed
+// #define IN1 26
+// #define IN2 27
+// #define IN3 14
+// #define IN4 12
+// #define ENB 13 //Left Motor Speed
+
+#define ENCODER1_PIN 34
+#define ENCODER2_PIN 35
+
+#define LED 2
+
+#define SERVO_PIN 32
+
+// ======== L298N =========
+int speedA = 0; // Speed for motor A (0 to 255) Right
+int speedB = 0; // Speed for motor B (0 to 255) Left
+
+bool reverseA = false; // Direction flag for motor A
+bool reverseB = false; // Direction flag for motor B
+
+unsigned long currentTime; //Time since boot
+unsigned long lastSerialUpdateTime = 0;
+//===========================
+
+// ========= Encoders =========
+
+// Constants for encoder resolution
+const float ENCODER_RESOLUTION = 40; // counts per rotation
+volatile int encoder1Count = 0;
+volatile int encoder2Count = 0;
+int lastEncoder1Count = 0;
+int lastEncoder2Count = 0;
+unsigned long lastInterruptTime1 = 0;
+unsigned long lastInterruptTime2 = 0;
+double wheelDiameter = 21.25; //Whell diameter is measured to be 21.25cm
+double wheel1Rotations = 0;
+double wheel2Rotations = 0;
+double wheel1Distance = 0; //Distance in cm
+double wheel2Distance = 0; //Distance in cm
+double wheelDistance = 0;
+
+//Hoisted for callbacks
+void handleEncoder1Interrupt();
+void handleEncoder2Interrupt();
+
+//===========================
+
+// ========= MPU9250 =========
+/* Mpu9250 object */
+bfs::Mpu9250 imu;
+
+/* Variables for yaw calculation */
+double yaw = 0.0;
+unsigned long lastTimeMpu;
+double gyroBiasZ = -0.019000; // Caluclated with repeated data collection average
+
+// PID constants
+double desiredYaw = 0.0; // Target yaw angle
+double Kp = 0.6; //2.5;
+double Ki = 0.6; //0.5;
+double Kd = 0.6; //1;
+double integral = 0;
+double lastError = 0;
+double dt = 0.1; // Time interval for PID calculation
+unsigned long lastTime = 0;
+unsigned long lastPidUpdateTime = 0;
+int minPidSpeed = 50; //60;
+int maxPidSpeed = 170; //180;
+
+//Turning Variables
+double turnTargetYaw = 0.00;
+int turnDirection;
+//PID constants for turning.
+double turnKp = 0.4;
+double turnKi = 0.4;
+double turnKd = 1.5;
+int minTurnSpeed = 60;
+int maxTurnSpeed = 150;
+int turning = 0; //Context switch. Change to more sophisticated method later
+//int turnSpeedA = 0; //Caclulation variables. Probably unnecessary
+//int turnSpeedB = 0; //Caclulation variables. Probably unnecessary
+int turnBaseSpeed = 60; //Speed for turning. 100 for 7v+ (2x 3.7v). 60 for 12v+ (3x 3.7v). 30 with PCB as low wire resistance
+double turnError;
+double turnDerivative;
+double turnIntegral = 0;
+double lastTurnError = 0;
+double turnPidOutput;
+bool overshootMode = false;
+//unsigned long lastTimeTurned = 0; //Probably unnecessary
+unsigned long lastTurnPidTime = 0;
+double turnErrorMargin = 1;
+unsigned long turnCompletionTime = 0;
+bool checkingOvershoot = false;
+double overshootMargin = 2;
+//===========================
+
+// ========= Distance moving =========
+//Distance Moving Variables
+bool distanceMoving = false; //Context switch flag.
+bool correctingTurnGap = false; //Cotenxt swith flag.
+bool correctingStopRotation = false; //Cotenxt swith flag.
+double targetDistance = 0;
+double distanceMoveOvershoot = 3; //Found thorugh repeated experiment
+double remaigningDistance = 0;
+int movingBaseSpeed = 90;
+
+
+//===========================
+
+// ========= Command List (Context switches etc) =========
+Ticker ticker; //Ticker instance for timed calls
+bool executingCommandList = false;
+
+//Hosited for callbacks
+void runCurrentCommand(String command);
+void handleInput(String input);
+//===========================
+
+
+void setup() {
+  Serial.begin(115200);
+  // Wait a moment for serial communication to stabilize
+  delay(100);
+
+  //Initial commandList
+  String initialCommand = "T90 T-90 G50";
+  setCommandList(initialCommand);
+
+  //Assign PWM channel to ENA, ENB by using analogWrite().
+  //Do this before attaching PWM to servo.
+  //Attaching servo a PWM channel before analogWrite() might cause analogWrite() to attach the same PWM channel and clock used by servo to ENA,ENB pins when it is called later in the program.
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+
+  //Call servoSetup after attaching PWM to ENA, ENB pins
+  servoSetup();
+
+  pinMode(LED,OUTPUT);
+  delay(500);
+  digitalWrite(LED,HIGH);
+
+  // =============== Marquer WEB ===============
+  setupMarquerWeb();
+
+  delay(500);
+  digitalWrite(LED,LOW);
+
+  // Set the callback function to handle POST messages
+  setWebMessageCallback([](String message) {
+    Serial.println("Received message via POST:");
+    Serial.println(message);
+    // Input Handling
+    handleInput(message);
+  });
+
+  // =============== Marquer WEB END ===============
+
+  //========= L298N =========
+  // Set all motor control pins as outputs
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  // Enable the motors
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+
+  // Initialize motors in stopped state
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  //===========================
+
+  //========= Encoders =========
+  // Configure encoder pins as inputs
+  pinMode(ENCODER1_PIN, INPUT);
+  pinMode(ENCODER2_PIN, INPUT);
+
+  // Attach interrupts to encoder pins
+  attachInterrupt(digitalPinToInterrupt(ENCODER1_PIN), handleEncoder1Interrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER2_PIN), handleEncoder2Interrupt, RISING);
+  //===========================
+
+  // ========= MPU9250 =========
+  /* Start the I2C bus */
+  Wire.begin();
+  Wire.setClock(400000);
+  /* I2C bus,  0x68 address */
+  imu.Config(&Wire, bfs::Mpu9250::I2C_ADDR_PRIM);
+  /* Initialize and configure IMU */
+  if (!imu.Begin()) {
+    Serial.println("Error initializing communication with IMU");
+    while(1) {}
+  }
+  /* Set the sample rate divider */
+  if (!imu.ConfigSrd(19)) {
+    Serial.println("Error configured SRD");
+    while(1) {}
+  }
+  // Initialize time
+  lastTimeMpu = millis();
+  //===========================
+
+  ledBlink(5);
+}
+
+void loop() {
+  currentTime = millis();
+
+  //========= MPU 9250 =========
+  calculateYaw();
+  //===========================
+
+
+  //=========== PID ===========
+  //Do PID per 50ms
+  if (currentTime - lastPidUpdateTime >= 50){
+    moveStraightPid();
+  }
+  //===========================
+
+  //========= Turn =========
+  if (turning == 1){
+    //Serial.println("Hit MainLoop");
+    turn();
+  }
+  //===========================
+
+  //========= Distance moving =========
+  if (distanceMoving){
+    //Serial.println("Hit MainLoop");
+    distanceMove();
+  }
+  if (correctingTurnGap && !turning){
+    //Serial.println("Hit MainLoop");
+    correctTurnGap();
+  }
+  //===========================
+
+  //========= L298N =========
+  // Control motor direction and speed
+  setSpeedAndDir();
+  //===========================
+
+
+  //========= Encoder =========
+  encoderCaclculations();
+  //===========================
+
+  // Once every 200ms stuff
+  if (currentTime - lastSerialUpdateTime >= 400) {
+    loopMarquerWeb();
+
+    float encoder1Speed = (encoder1Count - lastEncoder1Count) * (1000.0 / ENCODER_RESOLUTION); // Rotations per second
+    float encoder2Speed = (encoder2Count - lastEncoder2Count) * (1000.0 / ENCODER_RESOLUTION); // Rotations per second
+
+    // Send data to Serial Plotter in a single line with comma-separated values
+    Serial.print("Encoder1Speed:"); Serial.print(encoder1Speed);Serial.print(", ");
+    Serial.print("Encoder2Speed:"); Serial.print(encoder2Speed);Serial.print("\t");
+
+    // Print out the yaw value
+    Serial.print("Yaw: "); Serial.print(yaw,6);
+    Serial.print("\n");
+
+    lastEncoder1Count = encoder1Count;
+    lastEncoder2Count = encoder2Count;
+    lastSerialUpdateTime = currentTime;
+
+    //WebPrint("sR:"+String(speedA));
+    //WebPrint(", sL:"+String(speedB));
+    //WebPrint(", Te:"+String(turnError));
+    //WebPrint(", Ti:"+String(turnIntegral));
+    //WebPrint(", Td:"+String(turnDerivative));
+    //WebPrintln(", tPid:"+String(turnPidOutput));
+    //WebPrint(", EncL:"+String(wheel1Rotations));
+    //WebPrint(", WncR:"+String(wheel2Rotations));
+    // WebPrint(", D1:"+String(wheel1Distance));
+    // WebPrint(", D2:"+String(wheel2Distance));
+    // WebPrint(", DT:"+String(wheelDistance));
+    // WebPrint(", yaw:"+String(yaw));
+
+    WebPrintln(
+                "sL:"+String(speedB)+
+                ", sR:"+String(speedA)+
+                ", DT:"+String(wheelDistance)+
+                ", yaw:"+String(yaw)
+              );
+  }
+
+  //Check for serial input
+  if (Serial.available() > 0) {  // Check if there is any serial input available
+    String input = Serial.readStringUntil('\n');  // Read the input until newline character
+    handleInput(input);  // Pass the input to the handleInput function
+  }
+
+  //delay(1); // Delays are not used anymore!
+}
+
+void distanceMove(){
+  int slowingDistance=10; //Slow down from last 20cm
+
+  remaigningDistance = targetDistance-wheelDistance;
+
+  if (wheelDistance < targetDistance){
+    if (speedA < minPidSpeed){
+      speedA = movingBaseSpeed; //Make speed a bit more than pid baseline
+    }
+    if (speedB < minPidSpeed){
+      speedB = movingBaseSpeed; //Make speed a bit more than pid baseline
+    }
+
+    if (remaigningDistance<slowingDistance){
+      //Limit speed proportionately to remaigning distance at last 20cm.
+      double speedAMultiplier = constrain(remaigningDistance/slowingDistance, 0.8, 1);
+      double speedBMultiplier = constrain(remaigningDistance/slowingDistance, 0.8, 1);
+      
+      //Don't let speed get too low
+      speedA = constrain(speedA*speedAMultiplier, minPidSpeed , maxPidSpeed);
+      speedB = constrain(speedB*speedBMultiplier, minPidSpeed , maxPidSpeed);
+    }
+
+  } else {
+    //Active break
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, HIGH);
+    delay(10);
+    //Halt after moving
+    speedA = 0;
+    speedB = 0;
+    analogWrite(ENA, speedA);
+    analogWrite(ENB, speedB);
+    reverseA = false;
+    reverseB = false;
+    delay(100);
+    //Print debug stuff then set flag
+    WebPrint("Moved a distance of "+String(wheelDistance)+"cm.");
+    distanceMoving = false;
+    
+    //Enable turn gap correcting mode
+    correctingStopRotation = true; //For correcting stop rotation
+  
+    //reset distance
+    encoder1Count = 0;
+    encoder2Count = 0;
+    lastEncoder1Count = 0;
+    lastEncoder2Count = 0;
+    wheelDistance = 0;
+    //Set new target
+    targetDistance = 11;
+
+    //Lift pen
+    penUp();
+
+    //Correct turn error
+    WebPrintln("Correcting stop rotation of " + String(yaw));
+    double yawCorrection = 0; //yaw * -1;
+    //handleInput("T"+String(yawCorrection));
+    String argument = "T"+String(yawCorrection);
+    ticker.once_ms(500, std::bind(handleInput, argument)); //Wait till pen is up
+
+    //No need to execute next command as turning system does it after correcting turn gap, then correcting yaw
+    // if (executingCommandList) {
+    //   nextCommand();
+    // }
+  }
+}
+
+//Reverse distance move for clearing gap
+void correctTurnGap(){
+  int slowingDistance=10; //Slow down from last 20cm
+
+  reverseA = true;
+  reverseB = true;
+
+  remaigningDistance = targetDistance-wheelDistance;
+
+  if (wheelDistance < targetDistance){
+    if (speedA < minPidSpeed){
+      speedA = movingBaseSpeed; //Make speed a bit more than pid baseline
+    }
+    if (speedB < minPidSpeed){
+      speedB = movingBaseSpeed; //Make speed a bit more than pid baseline
+    }
+
+    if (remaigningDistance<5){
+      //Limit speed proportionately to remaigning distance at last 20cm.
+      double speedAMultiplier = constrain(remaigningDistance/5, 0.8, 1);
+      double speedBMultiplier = constrain(remaigningDistance/5, 0.8, 1);
+      
+      //Don't let speed get too low
+      speedA = constrain(speedA*speedAMultiplier, minPidSpeed , maxPidSpeed);
+      speedB = constrain(speedB*speedBMultiplier, minPidSpeed , maxPidSpeed);
+    }
+
+  } else {
+    //Active break
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, HIGH);
+    delay(10);
+    //Halt after moving
+    speedA = 0;
+    speedB = 0;
+    analogWrite(ENA, speedA);
+    analogWrite(ENB, speedB);
+    reverseA = false;
+    reverseB = false;
+    delay(100);
+    //Print debug stuff then set flag
+    WebPrintln("Turn gap compensation made. Moved a distance of "+String(wheelDistance)+"cm.");
+    correctingTurnGap = false;
+
+    //Correct turn error
+    WebPrintln("Correcting stop rotation of " + String(yaw));
+    double yawCorrection = 0; //yaw*-1;
+    handleInput("T"+String(yawCorrection));
+
+    //No need to execute next command as turning system does it after correcting yaw
+    // if (executingCommandList) {
+    //   nextCommand();
+    // }
+  }
+}
---
+++ Main Code/marquer14_L298N16_EncoderServoCommandlistsDistancePid/motion.ino
@@ -0,0 +1,248 @@
+/*
+Contain all L298n, MPU9250, PID and related functions
+*/
+
+
+// ========== MPU Calculations ==========
+//Calculate yaw and store in yaw variable
+void calculateYaw(){
+  //Get MPU9250 data if new data is available
+  if (imu.Read()) {
+    //Get gyroscope z-axis data and correct for bias
+    double gzRound = round((imu.gyro_z_radps() - gyroBiasZ) * 100.00) / 100.00; //With rounding
+    double gz = (gzRound) * (180.0 / PI); //Convert Radians per second to degrees per second
+
+    // Calculate Delta Time (dt)
+    unsigned long currentTimeMpu = millis();
+    double dtMpu = (currentTimeMpu - lastTimeMpu) / 1000.0;
+    lastTimeMpu = currentTimeMpu;
+
+    // Integrate gyroscope data for yaw
+    yaw += gz * dtMpu;
+  }
+}
+
+//Run Move Straight PID algorithm and set global speed values
+void moveStraightPid(){
+  dt = (currentTime - lastTime) / 1000.0; // Convert to seconds. Comment to use constant time
+
+  // PID calculations for yaw control
+  double error = desiredYaw - yaw;
+  integral += error * dt;
+  double derivative = (error - lastError) / dt;
+
+  double pidOutput = Kp * error + Ki * integral + Kd * derivative;
+
+  //Change PID for reverse
+  if (reverseA && reverseB) {
+    pidOutput *= -1;
+  }
+
+  //Only apply PID if not turning
+  if (turning == 0){
+    // Adjust motor speeds based on PID output, if speed>20
+    if (speedA>20 && speedB>20) {
+      speedA = constrain(speedA - pidOutput, minPidSpeed, maxPidSpeed);
+      speedB = constrain(speedB + pidOutput, minPidSpeed, maxPidSpeed);
+    }
+  }
+
+  lastError = error;   
+  lastTime = currentTime;
+  lastPidUpdateTime = currentTime;
+  //WebPrintln("Hit!"+String(currentTime));
+}
+
+
+// ========== Motor applications ==========
+
+// Control motor direction and speed
+void setSpeedAndDir(){
+  analogWrite(ENA, speedA);
+  analogWrite(ENB, speedB);
+  
+  if (reverseA) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+  } else {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  }
+
+  if (reverseB) {
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  } else {
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
+}
+
+
+//Turn the robot to a specific orientation
+void turn() {
+  bool turned = false;
+  //Direction 0 - right. 1 - left
+  Serial.println("Hit turn() function"); 
+
+  // For testing, moved to global scope
+  // double turnError;
+  // double turnDerivative;
+  // double turnPidOutput;
+
+  double currentTime = millis();
+
+  if(currentTime-lastTurnPidTime >= 50)
+  {
+    //Turn PID
+    dt = (currentTime - lastTurnPidTime)/1000; //Convert to seconds. Otherwise, values are abnormally large!
+
+    turnError = abs(turnTargetYaw - yaw);
+    turnIntegral += turnError * dt; 
+    turnDerivative = (turnError - lastTurnError) / dt;
+
+    //Constrain the turn integral
+    turnIntegral = constrain(turnIntegral, -200, 200);
+
+    turnPidOutput = (turnKp * turnError) + (turnKi * turnIntegral) + (turnKd * turnDerivative);
+    lastTurnError = turnError;
+
+    lastTurnPidTime = currentTime;
+  }
+  
+
+  //Set turn direction. Yaw should be 0 at the beginning of the turn
+  if (turnTargetYaw > yaw) {
+    turnDirection = 0; //Right
+  } else {
+    turnDirection = 1; //Left
+  }
+
+  //Anti Motor Stall. Pause and give a massive speed suddenly if integral is too much (which means it stayed in once yaw for long)
+  if (0 && turnIntegral >= 180) { //0 Disabled for now
+    speedA = 0;
+    speedB = 0;
+    analogWrite(ENA, speedA);
+    analogWrite(ENB, speedB);
+    WebPrintln("Anti Stall System Activated!");
+    turnIntegral = 100; //Reduce the integral a bit
+    delay(100); //Wait a bit to recover
+  }
+
+  //Turn Right
+  if (turnDirection == 0) {
+    //Configure right motor to reverse
+    reverseA = true;
+    reverseB = false;
+  }
+  //Turn left
+  else if (turnDirection == 1) {
+    reverseA = false;
+    reverseB = true;
+  }
+
+  //Apply power
+  if(!checkingOvershoot){
+    speedA = constrain(turnBaseSpeed + turnPidOutput, minTurnSpeed, maxTurnSpeed);
+    speedB = constrain(turnBaseSpeed + turnPidOutput, minTurnSpeed, maxTurnSpeed);
+    analogWrite(ENA, speedA);
+    analogWrite(ENB, speedB);
+  }
+  //In overshoot mode, apply a constant power
+  if(overshootMode){
+    speedA = constrain(turnBaseSpeed*1.4 , 80, 180);
+    speedB = constrain(turnBaseSpeed*1.4, 80, 180);
+    analogWrite(ENA, speedA);
+    analogWrite(ENB, speedB);
+  }
+
+  //Check if target Yaw reached
+  if (yaw >= (turnTargetYaw - turnErrorMargin) && yaw <= (turnTargetYaw + turnErrorMargin)) {  
+    turned = true;
+  } else {
+    turned = false;
+  }
+
+  if (turned && !checkingOvershoot) {
+    overshootMode = false; //Set the overshoot mode to false if not set yet
+    //Active break
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, HIGH);
+    delay(10);
+    //Halt after turning
+    speedA = 0;
+    speedB = 0;
+    analogWrite(ENA, speedA);
+    analogWrite(ENB, speedB);
+    reverseA = false;
+    reverseB = false;
+    //yaw = 0;
+    turnIntegral = 0;
+    lastTurnError = 0;
+
+    //Start checking for overshoot after some time
+    turnCompletionTime = millis();
+    checkingOvershoot = true;
+  }
+
+  if (checkingOvershoot) {
+    //Check for overshoot after 1 second
+    if (millis() - turnCompletionTime >= 1000) {
+      if (yaw > turnTargetYaw + overshootMargin || yaw < turnTargetYaw - overshootMargin) {
+        overshootMode = true;
+        Serial.println("Overshoot detected!");
+        WebPrintln("Target yaw: " + String(turnTargetYaw));
+        WebPrintln("Lower margin: " + String(turnTargetYaw-overshootMargin));
+        WebPrintln("Upper margin: " + String(turnTargetYaw+overshootMargin));
+        WebPrintln("Current yaw: " + String(yaw));
+        WebPrintln("Significant Overshoot Detected. Retrying!");
+      } else {
+        Serial.println("No considerable overshoot.");
+        WebPrintln("No considerable overshoot.");
+        //Finalize turning
+        Serial.println("Turned!");
+        WebPrintln("Turned!\nTurned yaw is: " + String(yaw));
+        //Halt after turning
+        speedA = 0;
+        speedB = 0;
+        analogWrite(ENA, speedA);
+        analogWrite(ENB, speedB);
+        //Release any break
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+        reverseA = false;
+        reverseB = false;
+        //Reset Variables
+        turnIntegral = 0;
+        lastTurnError = 0;
+        overshootMode = false;
+        turning = 0;
+        //Reverse distance moving is set when correcting turn gap
+        if (executingCommandList && !correctingStopRotation) {
+          //nextCommand(); // This will be done in correctAfterTurnGap()
+          
+          //reset distance
+          encoder1Count = 0;
+          encoder2Count = 0;
+          lastEncoder1Count = 0;
+          lastEncoder2Count = 0;
+          wheelDistance = 0;
+          //Set new target
+          targetDistance = 11;
+
+          //Set flasg for running after turn gap
+          correctingAfterTurnGap = true;
+        }
+        if (correctingStopRotation) {
+          correctingStopRotation = false;
+          correctingTurnGap = true;
+        }
+      }
+      checkingOvershoot = false; // Stop checking after the first check
+    }
+  }
+}

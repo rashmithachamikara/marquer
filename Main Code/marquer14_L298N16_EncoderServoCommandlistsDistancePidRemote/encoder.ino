e6aaba0c6b594bb7684346cfbeb968c03b7542a3
rashmithachamikara <rashmitha.chamickara@gmail.com>
2024-07-20 06:28:34 +0530

    Improved setSpeedandDir to be more responsive to directions. Encoders now count backwards

---
+++ Main Code/marquer14_L298N16_EncoderServoCommandlistsDistancePidRemote/encoder.ino
@@ -0,0 +1,41 @@
+//Encoder1 interrupts
+void IRAM_ATTR handleEncoder1Interrupt() {
+  unsigned long interruptTime = millis();
+  if (interruptTime - lastInterruptTime1 > 5){
+    //Reduce count if reversing
+    if(reverseA && reverseB){
+      if(encoder1Count>0){
+        encoder1Count--;
+      }
+    } else {
+      encoder1Count++;
+    }
+    lastInterruptTime1 = interruptTime;
+  }
+
+}
+
+//Encoder2 interrupts
+void IRAM_ATTR handleEncoder2Interrupt() {
+  unsigned long interruptTime = millis();
+  if (interruptTime - lastInterruptTime2 > 5){
+    //Reduce count if reversing
+    if(reverseA && reverseB){
+      if(encoder2Count>0){
+        encoder2Count--;
+      }
+    } else {
+      encoder2Count++;
+    }
+    lastInterruptTime2 = interruptTime;
+  }
+}
+
+//Do encoder caluclations including rotations and distances
+void encoderCaclculations(){
+  wheel1Rotations = encoder1Count/ENCODER_RESOLUTION;
+  wheel2Rotations = encoder2Count/ENCODER_RESOLUTION;
+  wheel1Distance = wheel1Rotations*wheelDiameter;
+  wheel2Distance = wheel2Rotations*wheelDiameter;
+  wheelDistance = (wheel1Distance+wheel2Distance)/2;
+}
